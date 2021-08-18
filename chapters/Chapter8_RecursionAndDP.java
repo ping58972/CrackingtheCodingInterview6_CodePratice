b15f3608eb4f201885ac01612c0bf670b0376353
ping58972 <ping58972@gmail.com>
2021-08-18 16:34:47 -0500

    finished 8.1

--- chapters/Chapter8_RecursionAndDP.java
+++ chapters/Chapter8_RecursionAndDP.java
@@ -14,8 +14,19 @@ public class Chapter8_RecursionAndDP {
     // int c2 = tripleStep(i);
     // System.out.println(i + ": " + c1 + " " + c2);
     // }
-    System.out.println(tripleStep(7));
-    System.out.println(tripleStep_topButtom(7));
+    // System.out.println(tripleStep(7));
+
+    int size = 5;
+    boolean[][] maze = AssortedMethods.randomBooleanMatrix(size, size, 70);
+
+    AssortedMethods.printMatrix(maze);
+
+    ArrayList<Point> path = getPath(maze);
+    if (path != null) {
+      System.out.println(path.toString());
+    } else {
+      System.out.println("No path found.");
+    }
   }
 
   /**
@@ -60,5 +71,69 @@ public class Chapter8_RecursionAndDP {
    * robot cannot step on them. Design an algorithm to find a path for the robot
    * from the top left to the bottom right.
    */
+  public static ArrayList<Point> getPath(boolean[][] maze) {
+    if (maze == null || maze.length == 0)
+      return null;
+    ArrayList<Point> path = new ArrayList<Point>();
+    HashSet<Point> failedPoints = new HashSet<Point>();
+    if (getPath(maze, maze.length - 1, maze[0].length - 1, path, failedPoints)) {
+      return path;
+    }
+    return null;
+  }
 
+  public static boolean getPath(boolean[][] maze, int row, int col, ArrayList<Point> path,
+      HashSet<Point> failedPoints) {
+    /* If out of bounds or not available, return. */
+    if (col < 0 || row < 0 || !maze[row][col]) {
+      return false;
+    }
+
+    Point p = new Point(row, col);
+
+    /* If we've already visited this cell, return. */
+    if (failedPoints.contains(p)) {
+      return false;
+    }
+
+    boolean isAtOrigin = (row == 0) && (col == 0);
+
+    /* If there's a path from the start to my current location, add my location. */
+    if (isAtOrigin || getPath(maze, row, col - 1, path, failedPoints)
+        || getPath(maze, row - 1, col, path, failedPoints)) {
+      path.add(p);
+      return true;
+    }
+
+    failedPoints.add(p); // Cache result
+    return false;
+  }
+}
+
+class Point {
+  public int row, column;
+
+  public Point(int row, int column) {
+    this.row = row;
+    this.column = column;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + row + ", " + column + ")";
+  }
+
+  @Override
+  public int hashCode() {
+    return this.toString().hashCode();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if ((o instanceof Point) && (((Point) o).row == this.row) && (((Point) o).column == this.column)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
